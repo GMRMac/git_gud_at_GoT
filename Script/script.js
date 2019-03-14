cd303ef95ec93f6e94df0a9a379f35dcfa7db568
GMRMac <32138360+GMRMac@users.noreply.github.com>
2019-03-14 11:43:52 +0100

    Made char selection for player 1
    added picture to selectable characters
    made it possible to select a character for preview

--- Script/script.js
+++ Script/script.js
@@ -8,10 +8,39 @@ fetch("https://anapioficeandfire.com/api/characters")
 })
 
 function displaySelectableCharacters(data){
+    let characterNumber = 0;
     let content = document.getElementById("caracterOverview");
     for(var characters of data){
-        content.innerHTML += "<div class='col-md-6 col-lg-4'><div class='caracterPreview'>"+"</div></div>";
+        let name = characters.aliases;
+        let pictureFileSplit = name[0].split(" ");
+        let pictureFileJoin = pictureFileSplit.join("_");
+        console.log(pictureFileJoin);
+        pictureFileJoin = "placeholder";
+        content.innerHTML += `
+<div onclick="findSelectedCharacter(`+characterNumber+`)" class='col-md-6 col-lg-4'><div class='caracterPreview'>
+<img style="width:100%;" src="Media/Pic/`+pictureFileJoin+`.jpg">
+<h2>`+name+`</h2></div></div>`;
+        characterNumber++;
     }
+}
+
+function findSelectedCharacter(number){
+    fetch("https://anapioficeandfire.com/api/characters/"+(number+1))
+    .then(function(data){
+        return data.json()
+    })
+    .then(function(myJson){
+        console.log(myJson);
+        displaySelectedCharacter(myJson);
+    })
+}
+
+function displaySelectedCharacter(data){
+    var character = document.getElementById("characterContent");
+    let name = data.aliases;
+    let pictureFileSplit = name[0].split(" ");
+    let pictureFileJoin = pictureFileSplit.join("_");
+    character.innerHTML = name;
 }
 
 console.log("hello world");
