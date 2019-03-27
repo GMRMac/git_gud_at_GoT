fee333d6e0050de9baefc3a0ead3e1e45647f607
GMRMac <32138360+GMRMac@users.noreply.github.com>
2019-03-27 10:30:12 +0100

    started on tiles
    started on tiles and log

--- Script/game.js
+++ Script/game.js
@@ -1,14 +1,25 @@
 function createGame(){
-    let content = document.getElementById("caracterOverview"),
+    let contentContainer = document.getElementById("caracterOverview"),
         h1 = document.querySelector("h1"),
         tiles = 30,
         tileNumber = 0;
+    contentContainer.innerHTML += "<div class='col-3' id='game__log'></div>";
+    contentContainer.innerHTML += "<div class='col-9' id='game__game'></div>";
+        let contentGame = document.getElementById("game__game"),
+            contentLog = document.getElementById("game__log");
+    console.log(content2);
     h1.innerHTML = "Game on!"
     content.innerHTML ="";
-    for(i = 0; 5 > i; i++){
+    for(i = 1; 6 > i; i++){
+        tileNumber2 = 4;
         for(j = 0; 6 > j; j++){
             tileNumber++;
-            content.innerHTML += "<div id='tile-" + tileNumber + "' class='tiles'></div>"
+            if(i%2){
+                contentGame.innerHTML += "<div class='col-2 game__tile' id='tile-" + (tileNumber+tileNumber2) + "' class='tiles'></div>"
+                tileNumber2 = tileNumber2 - 2;
+            } else {
+                contentGame.innerHTML += "<div class='col-2 game__tile' id='tile-" + tileNumber + "' class='tiles'></div>"
+            }
         }
     }
 }
