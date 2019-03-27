f59087001007ba6be3c1f1b38150d0a71c761ae9
GMRMac <32138360+GMRMac@users.noreply.github.com>
2019-03-27 12:26:48 +0100

    collumns are correct
    corrected a js error which made it crash.
    fixed the grid to make it 5x6 and not 6x5.

--- Script/game.js
+++ Script/game.js
@@ -3,22 +3,28 @@ function createGame(){
         h1 = document.querySelector("h1"),
         tiles = 30,
         tileNumber = 0;
-    contentContainer.innerHTML += "<div class='col-3' id='game__log'></div>";
-    contentContainer.innerHTML += "<div class='col-9' id='game__game'></div>";
+    contentContainer.innerHTML ="";
+    contentContainer.innerHTML += "<div class='col-sm-9 row' id='game__game'></div>";
+    contentContainer.innerHTML += "<div class='col-sm-3' id='game__log'></div>";
         let contentGame = document.getElementById("game__game"),
             contentLog = document.getElementById("game__log");
-    console.log(content2);
-    h1.innerHTML = "Game on!"
-    content.innerHTML ="";
-    for(i = 1; 6 > i; i++){
-        tileNumber2 = 4;
-        for(j = 0; 6 > j; j++){
+    h1.innerHTML = "Roll!"
+    for(i = 0; 6 > i; i++){
+        var tileNumber2 = 4;
+        for(j = 1; 6 > j; j++){
             tileNumber++;
             if(i%2){
-                contentGame.innerHTML += "<div class='col-2 game__tile' id='tile-" + (tileNumber+tileNumber2) + "' class='tiles'></div>"
+                contentGame.innerHTML += "<div class='col-2-10 game__tile col-2-tile' id='tile-" + (tileNumber+tileNumber2) + "' class='tiles'>" + (tileNumber+tileNumber2) + "</div>"
                 tileNumber2 = tileNumber2 - 2;
-            } else {
-                contentGame.innerHTML += "<div class='col-2 game__tile' id='tile-" + tileNumber + "' class='tiles'></div>"
+              /*  if(i === 5){
+                    console.log("turd")
+                    contentGame.innerHTML += "<div class='col-1'>";
+                }*/
+            }  else {
+                contentGame.innerHTML += "<div class='col-2-10 game__tile col-2-tile' id='tile-" + tileNumber + "' class='tiles'>" + tileNumber + "</div>"
+               /* if(i === 6){
+                    contentGame.innerHTML += "<div class='col-1'>";
+                }*/
             }
         }
     }
