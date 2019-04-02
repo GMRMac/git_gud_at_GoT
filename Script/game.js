acc1fad601230b025a60c32295fcb02f3dac3a09
GMRMac <32138360+GMRMac@users.noreply.github.com>
2019-04-02 12:28:51 +0200

    made game work
    -made game itself work ( still needs bugfixing in log, an ending, traps)
    -added log
    -added roll button

--- Script/game.js
+++ Script/game.js
@@ -1,5 +1,6 @@
 var heroTile = 1,
-    enemyTile = 1;
+    enemyTile = 1,
+    characterRound = "hero";
 
 function createGame(){
     let contentContainer = document.getElementById("caracterOverview"),
@@ -7,11 +8,11 @@ function createGame(){
         tiles = 30,
         tileNumber = 0;
     contentContainer.innerHTML ="";
-    contentContainer.innerHTML += "<div class='col-sm-9 row' id='game__container'><div class='col-sm-8 row' id='game__game'></div></div>";
-    contentContainer.innerHTML += "<div class='col-sm-3' id='game__log'></div>";
+    contentContainer.innerHTML += "<div class='col-sm-7 row' id='game__game'></div>";
+    contentContainer.innerHTML += "<div class='col-sm-5' id='game__log--container'><div class='col-12' id='game__button--container'><button id='game__button--roll' onclick='rollFunction()'>Roll me!</button></div><div class='col-12' id='game__log'></div>";
         let contentGame = document.getElementById("game__game"),
-            contentLog = document.getElementById("game__log");
-    h1.innerHTML = "Roll!"
+            contentLog = document.getElementById("game__log--container");
+    h1.innerHTML = "The race is on!"
     for(i = 0; 6 > i; i++){
         var tileNumber2 = 4;
         for(j = 1; 6 > j; j++){
@@ -25,6 +26,8 @@ function createGame(){
             if(j === 5 && i === 5){
                 moveCharacter("enemy");
                 moveCharacter("hero");
+                let gameLog = document.getElementById("game__log");
+                gameLog.innerHTML = "The game is on!<br>";
             }
         }
     }
@@ -38,5 +41,34 @@ function moveCharacter(character){
         findHeroTile.innerHTML += "<img id='heroImage' src='Media/Pic/placeholder.jpg'>";
     } else if (character === "enemy"){
         findEnemyTile.innerHTML += "<img id='enemyImage' src='Media/Pic/placeholder.jpg'>";
+    }
+}
+
+function rollFunction(){
+    let gameLog = document.getElementById("game__log"),
+        randomNumber = Math.ceil(Math.random()*6);
+    if (characterRound === "hero"){
+        let hero = document.getElementById("heroImage").remove();
+        gameLog.innerHTML += "Hero rolled a " + randomNumber + "<br>";
+        gameLog.innerHTML += "Hero moves from tile " + heroTile + " to tile " + (heroTile+randomNumber) + "<br>";
+        heroTile = (heroTile+randomNumber);
+        moveCharacter("hero");
+        if (randomNumber == 6){
+            return
+        } else {
+            return characterRound = "enemy";
+        }
+    }
+    if (characterRound == "enemy"){
+        let enemy = document.getElementById("enemyImage").remove();
+        gameLog.innerHTML += "Enemy rolled a " + randomNumber + "<br>";
+        gameLog.innerHTML += "Enemy moves from tile " + enemyTile + " to tile " + (enemyTile+randomNumber); + "<br>"
+        enemyTile = (enemyTile+randomNumber);
+        moveCharacter("enemy");
+        if (randomNumber == 6){
+            return
+        } else {
+            return characterRound = "hero";
+        }
     }
 }
