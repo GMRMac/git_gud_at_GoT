3fc850db1898251a117e3d9e88a844056e9c090c
GMRMac <32138360+GMRMac@users.noreply.github.com>
2019-04-03 11:40:55 +0200

    "finished"
    -"finished" the core game itself and now its re-playable without refreshing the page.
    -Fixed that the game crashes once one exceeds tile 30
    -Removed "game.js" as it cant get functions from "script.js". All content from game file is moved to script file.
    -Added victory and defeat page

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,14 @@
-fetch("https://anapioficeandfire.com/api/characters")
-.then(function(data){
-    return data.json();
-})
-.then(function(myJson){
-    console.log(myJson);
-    displaySelectableCharacters(myJson, "Select your hero!", "hero");
-})
+function fetchData(){
+    fetch("https://anapioficeandfire.com/api/characters")
+    .then(function(data){
+        return data.json();
+    })
+    .then(function(myJson){
+        console.log(myJson);
+        displaySelectableCharacters(myJson, "Select your hero!", "hero");
+    })
+}
+fetchData();
 
 var heroCharacter,
     enemyCharacter,
@@ -15,6 +18,7 @@ function displaySelectableCharacters(data, header, characterSide){
     let characterNumber = 0,
         content = document.getElementById("caracterOverview"),
         h1 = document.querySelector("h1");
+    content.innerHTML = "";
     if(h1.innerHTML == "Select your enemy!"){
         document.innerHTML ="";
         return createGame();
@@ -91,5 +95,110 @@ function compareValues(data, wanterResult){
         if(bits == wanterResult){
             return bits
         }
+    }
+}
+
+var heroTile = 1,
+    enemyTile = 1,
+    characterRound = "hero";
+
+function createGame(){
+    let contentContainer = document.getElementById("caracterOverview"),
+        h1 = document.querySelector("h1"),
+        tiles = 30,
+        tileNumber = 0;
+    heroTile = 1;
+    enemyTile = 1;
+    contentContainer.innerHTML ="";
+    contentContainer.innerHTML += "<div class='col-sm-7 row' id='game__game'></div>";
+    contentContainer.innerHTML += "<div class='col-sm-5' id='game__log--container'><div class='col-12' id='game__button--container'><button id='game__button--roll' onclick='rollFunction()'>Roll me!</button></div><div class='col-12' id='game__log'></div>";
+        let contentGame = document.getElementById("game__game"),
+            contentLog = document.getElementById("game__log--container");
+    h1.innerHTML = "The race is on!"
+    for(i = 0; 6 > i; i++){
+        var tileNumber2 = 4;
+        for(j = 1; 6 > j; j++){
+            tileNumber++;
+            if(i%2){
+                contentGame.innerHTML += "<div class='col-2-10 game__tile col-2-tile' id='tile-" + (tileNumber+tileNumber2) + "' class='tiles'>"/* + (tileNumber+tileNumber2)*/ + "</div>";
+                tileNumber2 = tileNumber2 - 2;
+            }  else {
+                contentGame.innerHTML += "<div class='col-2-10 game__tile col-2-tile' id='tile-" + tileNumber + "' class='tiles'>"/* + tileNumber*/ + "</div>";
+            }
+            if(j === 5 && i === 5){
+                moveCharacter("enemy");
+                moveCharacter("hero");
+                let gameLog = document.getElementById("game__log");
+                gameLog.innerHTML = "The game is on!<br>";
+            }
+        }
+    }
+}
+
+function moveCharacter(character){
+    if(heroTile > 30){
+        return winnerScreen("hero");
+    } else if (enemyTile > 30) {
+        return winnerScreen("enemy");
+    }
+    let findHeroTile = document.getElementById("tile-"+heroTile),
+        findEnemyTile = document.getElementById("tile-"+enemyTile);
+    console.log(enemyTile,heroTile);
+    if (character === "hero"){
+        findHeroTile.innerHTML += "<img id='heroImage' src='Media/Pic/placeholder.jpg'>";
+    } else if (character === "enemy"){
+        findEnemyTile.innerHTML += "<img id='enemyImage' src='Media/Pic/placeholder.jpg'>";
+    }
+}
+
+function rollFunction(){
+    let gameLog = document.getElementById("game__log"),
+        randomNumber = Math.ceil(Math.random()*6);
+    if (characterRound === "hero"){
+        let hero = document.getElementById("heroImage").remove();
+        gameLog.innerHTML += "Hero rolled a " + randomNumber + "<br>";
+        gameLog.innerHTML += "Hero moves from tile " + heroTile + " to tile " + (heroTile+randomNumber) + "<br>";
+        heroTile = (heroTile+randomNumber);
+        moveCharacter("hero");
+        if (randomNumber == 6){
+            return
+        } else {
+            return characterRound = "enemy";
+        }
+    }
+    if (characterRound == "enemy"){
+        let enemy = document.getElementById("enemyImage").remove();
+        gameLog.innerHTML += "Enemy rolled a " + randomNumber + "<br>";
+        gameLog.innerHTML += "Enemy moves from tile " + enemyTile + " to tile " + (enemyTile+randomNumber); + "<br>"
+        enemyTile = (enemyTile+randomNumber);
+        moveCharacter("enemy");
+        if (randomNumber == 6){
+            return
+        } else {
+            return characterRound = "hero";
+        }
+    }
+}
+
+function winnerScreen(character){
+    let contentContainer = document.getElementById("caracterOverview"),
+        h1 = document.querySelector("h1");
+    contentContainer.innerHTML="";
+    if(character == "hero"){
+        h1.innerHTML = "Victory!";
+        contentContainer.innerHTML = `
+<h2>Congratulation on your very win, you managed to reach the goal before your enemy!</h2>
+<p>Want to try beat the enemy again? Click button below!</p>
+<button class="victoryPageButton" onclick="fetchData()">Restart the game</button>
+
+`
+    } else if (character == "enemy"){
+        h1.innerHTML = "Defeat.";
+        contentContainer.innerHTML = `
+<h2>You did not manage to reach the goal before the enemy and there for you lost, better luck next time!</h2>
+<p>Want to get that bitter sweet revenge? Click the button below!</p>
+<button class="victoryPageButton" onclick="fetchData()">Restart the game</button>
+
+`
     }
 }
